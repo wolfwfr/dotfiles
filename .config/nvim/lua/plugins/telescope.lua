fd19e471d17c834356678e91d41d473c6a1b4ec1
Wolf Freije <wolffreije@innius.com>
2024-03-11 11:30:19 +0100

    feat [telescope]: use custom keymaps to navigate up and down in telescope windows

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,41 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
+    opts = function(_, opts)
+      local actions = require("telescope.actions")
+      opts.defaults = {
         border = true,
-      },
-      pickers = {
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-p>"] = actions.preview_scrolling_right,
+          },
+        },
+      }
+      opts.pickers = {
         find_files = {
           hidden = true,
         },
-      },
-    },
+      }
+    end,
+
+    -- opts = {
+    --   defaults = {
+    --     border = true,
+    --     -- mappings = {
+    --     --   i = {
+    --     --     ["<C-j>"] = "next_item",
+    --     --     ["<C-k>"] = "previous_item",
+    --     --     -- ["<C-p>"] = "",
+    --     --   },
+    --     -- },
+    --   },
+    --   pickers = {
+    --     find_files = {
+    --       hidden = true,
+    --     },
+    --   },
+    -- },
   },
 }
