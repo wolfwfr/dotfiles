e00ba60e118b1879420aeff8a4d17611ac4fcd4e
Wolf Freije <wolffreije@innius.com>
2024-03-11 07:37:56 +0100

    refactor [neo-tree]: re-enable hide of ignored files

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,15 +1,10 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    -- default_component_configs = {
-    --   name = {
-    --     use_git_status_colors = false,
-    --   },
-    -- },
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
-        hide_gitignored = false,
+        hide_gitignored = true,
         always_show = {
           ".gitignore",
           ".env",
