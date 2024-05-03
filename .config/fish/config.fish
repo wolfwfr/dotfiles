f4620a72997594bf746d3beb65a96f291cc4f5a0
Wolf Freije <wolffreije@innius.com>
2024-05-03 10:12:16 +0200

    feat [fish]: add and configure bat and eza

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,7 +18,12 @@ alias awscode="ykman oath accounts code aws"
 alias n="nvim ."
 alias ns="nvim" # ns=nvim session (auto-session will create or load a session)
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias bat="batcat"
+alias ls="eza --color=always --long --git"
+alias lss="eza --color=always --icons=always --long --no-filesize --no-time --no-user --no-permissions"
 
 thefuck --alias | source
 
 eval (tmuxifier init - fish)
+
+export BAT_THEME="kanagawa"
