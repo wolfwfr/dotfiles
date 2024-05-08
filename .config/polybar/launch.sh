d4765d41f8d601235144dd4f10c8765a708e5ed4
Wolf Freije <wolffreije@innius.com>
2024-05-08 09:13:44 +0200

    i3: polybar configuration with multi-monitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,48 @@
 #!/usr/bin/env bash
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+	MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+else
+	primary=$(xrandr --query | grep primary | cut -d" " -f1)
+
+	for m in $screens; do
+		if [[ $primary == $m ]]; then
+			MONITOR=$m TRAY_POS=right polybar secondary &
+		else
+			MONITOR=$m TRAY_POS=none polybar main &
+		fi
+	done
+fi
+
+# new section
+# killall -q polybar
+#
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+#
+# polybar monitor1 &
+# polybar monitor2 &
+# end of new section
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
+
+# polybar-msg cmd quit
+
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log &
-disown
 
-echo "Bars launched..."
+# echo "---" | tee -a /tmp/polybar1.log
+#
+# polybar bar 2>&1 | tee -a /tmp/polybar1.log &
+# disown
+#
+# echo "Bars launched..."
