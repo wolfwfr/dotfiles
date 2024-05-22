ddd7fd98244944b520efbdface9be714ee4a7f58
Wolf Freije <wolffreije@innius.com>
2024-05-22 12:11:56 +0200

    nvim: disable inlay hints

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,17 +3,17 @@
 -- Add any additional autocmds here
 
 -- fixes <tab> key going nuts in insert mode after using autocompletion functionality
-function leave_snippet()
-  if
-    ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
-    and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-    and not require("luasnip").session.jump_active
-  then
-    require("luasnip").unlink_current()
-  end
-end
-
--- stop snippets when you leave to normal mode
-vim.api.nvim_command([[
-    autocmd ModeChanged * lua leave_snippet()
-]])
+-- function leave_snippet()
+--   if
+--     ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+--     and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+--     and not require("luasnip").session.jump_active
+--   then
+--     require("luasnip").unlink_current()
+--   end
+-- end
+--
+-- -- stop snippets when you leave to normal mode
+-- vim.api.nvim_command([[
+--     autocmd ModeChanged * lua leave_snippet()
+-- ]])
