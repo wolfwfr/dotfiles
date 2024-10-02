5ddba166dfcc8785386a939d53f336b011d142fe
Wolf <wolffreije@innius.com>
2024-10-02 14:28:44 +0200

    feat [fish]: add zoxide init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,8 @@ set -x GO111MODULE on
 set -x AWS_REGION us-east-1
 set -x CHROME_BIN /snap/bin/chromium
 
+set -g fish_greeting
+
 alias dup="docker compose up -d"
 alias down="docker compose down"
 alias dops="docker ps"
@@ -18,7 +20,6 @@ alias awscode="ykman oath accounts code aws"
 alias n="nvim ."
 alias ns="nvim" # ns=nvim session (auto-session will create or load a session)
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-alias bat="batcat"
 alias ls="eza --color=always --long --git"
 alias lss="eza --color=always --icons=always --long --no-filesize --no-time --no-user --no-permissions"
 
@@ -36,3 +37,5 @@ thefuck --alias | source
 #eval (tmuxifier init - fish)
 
 export BAT_THEME="kanagawa"
+
+zoxide init fish | source
