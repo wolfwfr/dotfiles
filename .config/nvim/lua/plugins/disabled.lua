bd17c749f78b9a588f5ade0a1ff76b6260d8f168
Wolf <wolffreije@innius.com>
2025-03-05 10:52:11 +0100

    chore [nvim]: disable 3rd/image.nvim (image sticks around in tmux)

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   -- { "folke/flash.nvim", enabled = false },
   { "nvim-neotest/neotest-go", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  { "3rd/image.nvim", enabled = false },
 }
