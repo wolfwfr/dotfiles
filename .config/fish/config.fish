2d8ff6b676df1f3d87fc959a3ab8e22490da0266
Wolf <wolffreije@innius.com>
2025-07-14 07:36:00 +0200

    chore [fish]: add aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set -x TERM xterm-256color
 set -x GO111MODULE on
 set -x AWS_REGION us-east-1
 set -x CHROME_BIN /snap/bin/chromium
+# set -x JIRA_AUTH_TYPE bearer
 
 set -g fish_greeting
 
@@ -20,6 +21,7 @@ alias awscode="ykman oath accounts code aws"
 alias n="nvim ."
 alias ns="nvim" # ns=nvim session (auto-session will create or load a session)
 alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias lazydot="lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias ls="eza --color=always --long --git"
 alias lss="eza --color=always --icons=always --long --no-filesize --no-time --no-user --no-permissions"
 alias cnvim="rm /home/wolf/.local/state/nvim/shada/*"
@@ -30,11 +32,20 @@ alias ave="aws-vault exec admin"
 alias aves="aws-vault exec --server admin"
 alias avel="aws-vault exec admin --duration=8h"
 alias sourcefish="source ~/.config/fish/config.fish"
+alias jira='JIRA_API_TOKEN=$(pass show jira/api_token) /usr/bin/jira'
+alias yrc='ykman oath accounts code --single redis | wl-copy -n' # using wayland's clipboard
+alias yac='ykman oath accounts code --single aws | wl-copy -n' # using wayland's clipboard
+alias cl='clockify-cli'
+
+# credit: https://unix.stackexchange.com/questions/6910/there-must-be-a-better-way-to-replace-single-newlines-only
+# if line starts (^) with end of line ($); print newline; if any character (.) print
+# the whole line ($0) and append with space.
+alias trimendnl='awk \' /^$/ { print "\n"; } /./ { printf("%s ", $0); } \''
 
 # redshifting for wayland
 alias wlreset="pidof \"wlsunset\" | xargs kill"
 function wlred
-    wlsunset -t 4500 &
+    wlsunset -T 4500 &
 end
 
 function yy
