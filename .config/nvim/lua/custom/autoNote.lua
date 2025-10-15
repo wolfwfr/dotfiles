f09dbc6bd9c34871fd2d37825e51c6aea95f6b24
Wolf <wolffreije@innius.com>
2025-10-15 11:54:59 +0200

    feat [autoNote]: add quick location check for personal convenience

--- .config/nvim/lua/custom/autoNote.lua
+++ .config/nvim/lua/custom/autoNote.lua
@@ -25,7 +25,7 @@ local function makeNote(dirname)
   local filename = time .. " [" .. dayconv .. "]" .. ".md"
 
   if dirname ~= journalDir then
-    local confirm = vim.fn.confirm("custom filename?", "&y\n&n", "n")
+    local confirm = vim.fn.confirm("custom filename?", "&y\n&n", 2)
     if confirm == 1 then
       local fn_override = vim.fn.input("filename: ")
       filename = fn_override
@@ -42,7 +42,17 @@ local function makeNote(dirname)
   vim.cmd("e " .. fullpath) -- open file
 
   if vim.fn.getfsize(fullpath) == 0 and filename:match(".*.md") then
-    vim.api.nvim_input("i# ")
+    local content = ""
+    if dirname == journalDir then
+      local confirm = vim.fn.confirm("at the office?", "&y\n&n", 2)
+      if confirm == 1 then
+        content = "journal\n\nOFFICE\n\n"
+      else
+        content = "journal\n\nHOME\n\n"
+      end
+    end
+
+    vim.api.nvim_input("i# " .. content)
   end -- enter insert mode & start file with a heading
 end
 
