39b9feed225983ac88fb68141f2ef7d01f537331
Wolf <wolffreije@innius.com>
2025-12-15 10:57:52 +0100

    fix [nvim]: replace markdown-preview.nvim build command

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -4,14 +4,7 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function(plugin)
-      if vim.fn.executable("npx") then
-        vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
-      else
-        vim.cmd([[Lazy load markdown-preview.nvim]])
-        vim.fn["mkdp#util#install"]()
-      end
-    end,
+    build = ":call mkdp#util#install()",
     init = function()
       if vim.fn.executable("npx") then
         vim.g.mkdp_filetypes = { "markdown" }
