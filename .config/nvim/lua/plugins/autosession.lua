fc45015178d31212693f4f5db124a4a2509c6ca7
Wolf <wolffreije@innius.com>
2026-02-23 07:36:07 +0100

    chore [nvim]: autosession.lua config syntax migration

--- .config/nvim/lua/plugins/autosession.lua
+++ .config/nvim/lua/plugins/autosession.lua
@@ -2,16 +2,23 @@ return {
   {
     "rmagatti/auto-session",
     opts = {
+
+      --
+      auto_restore = true,
+      auto_save = true,
+      enabled = true,
+      git_use_branch_name = true,
       log_level = "error",
-      auto_session_enabled = true,
-      auto_save_enabled = true,
-      auto_restore_enabled = true,
-      auto_session_use_git_branch = true,
-      auto_session_suppress_dirs = { "~/" },
+      suppressed_dirs = { "~/" },
 
       -- ensures that neotree opens by default upon restore
       pre_save_cmds = { "Neotree close" },
       post_restore_cmds = { "Neotree filesystem show" },
+      -- post_restore_cmds = {
+      --   function()
+      --     require("snacks.explorer").open({ enter = false })
+      --   end,
+      -- },
     },
   },
 }
