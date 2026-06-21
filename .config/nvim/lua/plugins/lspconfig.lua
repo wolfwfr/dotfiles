533207ba9f6117e619702f79882bf008f410607d
Wolf <wolffreije@innius.com>
2026-06-21 19:38:15 +0200

    chore [nvim]: some tweaking/experimenting

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,21 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      settings = {
+        ["*"] = {
+          keys = {
+            {
+              "<c-k>",
+              function()
+                return vim.lsp.buf.signature_help()
+              end,
+              mode = "n",
+              desc = "Signature Help",
+              has = "signatureHelp",
+            },
+          },
+        },
+      },
       diagnostics = {
         virtual_text = false,
       },
