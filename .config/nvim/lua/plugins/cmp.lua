8b8e7ed5e4838b04140f6d3225f7de5f00b10f1c
Wolf <wolffreije@innius.com>
2026-06-21 19:24:54 +0200

    fix [cmp]: fix super-tab in new version

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -18,7 +18,32 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      preset = "super-tab",
+      -- preset = "super-tab",
+      preset = "none", -- below is adapted from preset 'super-tab'
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+      ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+      -- ["<C-k>"] = { "fallback" },
     },
     sources = {
       per_filetype = {
