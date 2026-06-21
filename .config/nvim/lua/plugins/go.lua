f3c1faba5b75814168b0d87cd63b2d46ed7a1c39
Wolf <wolffreije@innius.com>
2026-06-21 19:31:42 +0200

    chore [nvim]: remove obsolete go config autocmd

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -38,14 +38,6 @@ return {
           },
         },
       })
-      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.go",
-        callback = function()
-          require("go.format").goimports()
-        end,
-        group = format_sync_grp,
-      })
       return {
         -- lsp_keymaps = false,
         -- other options
